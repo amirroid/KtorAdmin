722ca51113d6228d81dbe79854c25a695d8a5834
Amirreza Gholami <jv233114@gmail.com>
2025-02-03 01:59:39 +0330

    Add expand and shrink sidebar

--- KtorAdminLibrary/src/main/resources/static/js/common.js
+++ KtorAdminLibrary/src/main/resources/static/js/common.js
@@ -1,3 +1,8 @@
+function hasExpandedClassInSidebar(){
+    const icon = document.getElementById("menu-expand-icon");
+    return icon.classList.contains("expanded")
+}
+
 function handleMenuHover() {
     const menu = document.querySelector('.menu');
     const sidebar = document.querySelector('.sidebar');
@@ -19,7 +24,7 @@ function handleMenuHover() {
     });
 
     document.body.addEventListener('mouseover', function (event) {
-        if (!sidebar.contains(event.target) && !menu.contains(event.target)) {
+        if (!sidebar.contains(event.target) && !menu.contains(event.target) && !hasExpandedClassInSidebar()) {
             sidebar.style.left = '-300px';
             isSidebarVisible = false;
         }
@@ -32,7 +37,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 function toggleFilter() {
-    let topBox = document.getElementById("top-box");
+    var topBox = document.getElementById("top-box")
     let row = document.getElementById("actions-row")
     let filters = document.getElementById("filters-container")
     if (filters.classList.contains("show")) {
@@ -59,7 +64,26 @@ function closeFilters() {
         topBox.classList.remove("show-filters")
         topBox.style.height = "55px"
         row.classList.remove("hide")
-    }else {
-        redirectToAdd()
+    }
+}
+
+
+function expandOrShrinkSidebar() {
+    const icon = document.getElementById("menu-expand-icon");
+    const container = document.getElementById("container");
+    const sidebar = document.getElementsByClassName("sidebar")[0];
+    const menuIcon = document.getElementsByClassName("menu")[0];
+    if (icon.classList.contains("expanded")) {
+        icon.classList.remove("expanded");
+        container.style.width = "100%";
+        container.style.marginLeft = "0";
+        menuIcon.classList.remove("shrink");
+    } else {
+        let sidebarRect = sidebar.getBoundingClientRect();
+        icon.classList.add("expanded");
+        console.log(screen.width - sidebarRect.width - 32)
+        menuIcon.classList.add("shrink");
+        container.style.marginLeft = (16 + sidebarRect.width).toString() + "px";
+        container.style.width = `${screen.width - sidebarRect.width - 48}px`;
     }
 }
